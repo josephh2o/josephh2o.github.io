7f6b1c69e59b3fe2a628e255b9fda25d17f3e6c0
Joseph Ho <joseph.h2o.273.15k@gmail.com>
2024-11-23 20:10:09 -0600

    adjusted base

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
+  base: 'https://josephh2o.github.io/',
   plugins: [react()],
   server: {
     host: '0.0.0.0', // This will expose the server to your network
