c4f3797d3a6459f5c3f3072c9cd7a72f087a262d
Joseph Ho <joseph.h2o.273.15k@gmail.com>
2024-11-23 20:39:13 -0600

    Moved from BrowserRouter to HashRouter to prevent 404

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from './layout';
 import Home from './pages/home';
 import Resume from './pages/resume';
