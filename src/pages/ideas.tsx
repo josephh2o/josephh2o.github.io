7271d9c14ad1aca4be4a9269f1a71d8bac713ebe
Joseph Ho <joseph.h2o.273.15k@gmail.com>
2025-04-24 00:38:02 -0700

    update error on homepage

--- src/pages/ideas.tsx
+++ src/pages/ideas.tsx
@@ -3,7 +3,7 @@ import '../css/ideas.css'
 function Ideas() {
     return (
         <div className ="home-container">
-            <p className="introduction"> Loading thoughts</p>
+            <p className="placeholder"> Loading thoughts</p>
         </div>
     );
 }
